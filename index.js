c4d093a63e8d57670825d63d8f7fd85c0e721492
Daniel Maike <danmke@hotmail.com>
2021-10-26 20:25:48 -0300

    Cleaning code
    Signed-off-by: Daniel Maike <danmke@hotmail.com>

--- index.js
+++ index.js
@@ -12,27 +12,12 @@ const server = app.listen(PORT, function () {
 // Socket setup
 const io = socket(server);
 
-const activeUsers = new Set();
-
 io.on("connection", function (socket) {
   console.log("Made socket connection");
 
-  socket.on("new user", function (data) {
-    socket.userId = data;
-    activeUsers.add(data);
-    io.emit("new user", [...activeUsers]);
-  });
-
-  socket.on("disconnect", () => {
-    activeUsers.delete(socket.userId);
-    io.emit("user disconnected", socket.userId);
-  });
-
-  socket.on("chat message", function (data) {
-    io.emit("chat message", data);
+  socket.on("message", function (data) {
+    io.emit("message", data);
+    console.log(message);
   });
   
-  socket.on("typing", function (data) {
-    socket.broadcast.emit("typing", data);
-  });
 });
