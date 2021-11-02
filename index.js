7bbbeac4db2ab34b2f23d6c62ea2a40ce2b121f3
Daniel Maike <danmke@hotmail.com>
2021-11-02 20:44:18 -0300

    Add publish and subscribe to register esp
    Co-authored-by: Rhuan <rhuancarlos.queiroz@gmail.com>

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 const express = require("express");
 const socket = require("socket.io");
-const cors = require('cors')
+const cors = require('cors');
 
-const PORT = 5000;
+var mqtt = require('mqtt');
+var clientMqtt = mqtt.connect('mqtt://localhost');
+
 const app = express();
+app.use(cors());
 
-app.use(cors())
+const PORT = 5005;
+const MATRICULA = "123456789";
+const BASE_TOPIC = `fse2021/${MATRICULA}/dispositivos/`;
+
+const BASE_TOPIC_REGEX = new RegExp(`^fse2021/123456789/dispositivos/([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$`);
+let esps = new Map();
 
 const server = app.listen(PORT, function () {
   console.log(`Listening on port ${PORT}`);
   console.log(`http://localhost:${PORT}`);
 });
 
-const io = socket(server);
+const io = socket(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 
 io.on("connection", function (socket) {
   console.log("Made socket connection");
@@ -20,11 +33,32 @@ io.on("connection", function (socket) {
   socket.on("register", function (data) {
     io.emit("register", data);
     console.log(data);
-  });
-
-  socket.on("message", function (data) {
-    io.emit("message", data);
-    console.log(data);
+    clientMqtt.publish(BASE_TOPIC + data.id, JSON.stringify({"local": data.local}));
+    esps.set(data.id, data);
+    console.log(esps);
   });
   
+});
+
+
+clientMqtt.on('connect', function () {
+  clientMqtt.subscribe(BASE_TOPIC + "+", function (err) {
+    if (err) {
+      console.log(err);
+    }
+  })
+});
+
+clientMqtt.on('message', function (topic, message) {
+  if (BASE_TOPIC_REGEX.test(topic)) {
+    let messageJson = JSON.parse(message.toString());
+    if (Object(messageJson).hasOwnProperty("id")) {
+      esps.set(messageJson.id, messageJson);
+      console.log(esps);
+      io.emit("new esp", messageJson);
+    }
+  }
+  console.log(topic);
+  console.log(message.toString());
+  // clientMqtt.end();
 });
