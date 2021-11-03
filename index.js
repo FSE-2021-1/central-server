100dc9996643d06cb54af0b417e5fb4e2924a59d
Rhuan Queiroz <rhuancarlos.queiroz@gmail.com>
2021-11-02 22:17:36 -0300

    Permitindo requisição de estado

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const socket = require("socket.io");
-const cors = require('cors');
+const cors = require("cors");
 
-const PoweredMap = require('./poweredmap.js');
+const PoweredMap = require("./poweredmap.js");
 
-var mqtt = require('mqtt');
-var clientMqtt = mqtt.connect('mqtt://broker-mosquitto');
+var mqtt = require("mqtt");
+var clientMqtt = mqtt.connect("mqtt://broker-mosquitto");
 
 const app = express();
 app.use(cors());
@@ -14,10 +14,12 @@ const PORT = 5005;
 const MATRICULA = "123456789";
 const BASE_TOPIC = `fse2021/${MATRICULA}/dispositivos/`;
 
-const BASE_TOPIC_REGEX = new RegExp(`^fse2021/123456789/dispositivos/([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$`);
+const BASE_TOPIC_REGEX = new RegExp(
+  `^fse2021/123456789/dispositivos/([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$`
+);
 
 let esps = new PoweredMap();
-esps.setUpdateCallback(function() {
+esps.setUpdateCallback(function () {
   io.emit("state", Array.from(esps.values()));
 });
 
@@ -38,23 +40,29 @@ io.on("connection", function (socket) {
 
   socket.on("register", function (data) {
     io.emit("register", data);
-    clientMqtt.publish(BASE_TOPIC + data.id, JSON.stringify({"local": data.local}));
+    clientMqtt.publish(
+      BASE_TOPIC + data.id,
+      JSON.stringify({ local: data.local })
+    );
     esps.set(data.id, data);
     console.log(`ESP32: ${data.id} registered!`);
   });
-  
+
+  socket.on("req state", function (socketId) {
+    console.log(`Front: ${socketId} requested state`);
+    socket.emit("state", Array.from(esps.values()));
+  });
 });
 
-
-clientMqtt.on('connect', function () {
+clientMqtt.on("connect", function () {
   clientMqtt.subscribe(BASE_TOPIC + "+", function (err) {
     if (err) {
       console.log(err);
     }
-  })
+  });
 });
 
-clientMqtt.on('message', function (topic, message) {
+clientMqtt.on("message", function (topic, message) {
   if (BASE_TOPIC_REGEX.test(topic)) {
     let messageJson = JSON.parse(message.toString());
     if (Object(messageJson).hasOwnProperty("id")) {
